8af27bb353175258fd3080ef0f562591169bd8e2
Shravan10 <shravankumarghanchi@gmail.com>
2021-02-26 20:01:58 +0530

    Day 1 React JS Static Website Demo
    React Js Website demo project.

---
+++ App.js
@@ -0,0 +1,16 @@
+  
+import './App.css';
+import Banner from "./components/banner";
+import Nav from "./components/Nav";
+
+
+function App() {
+  return (
+    <div>
+      <Banner/>
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
